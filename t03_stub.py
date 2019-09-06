b7c336406bbc806f80b1d4eb7133b54cdd5c1a76
morenoa2 <morenoa2@berea.edu>
2019-09-06 09:00:30 -0400

    Initial Commit

--- t03_stub.py
+++ t03_stub.py
@@ -17,25 +17,29 @@ def function_1():
     """
     Docstring for function_1
     """
-    pass
-    # ...
+    #We left off here...
 
 
 def function_2():
     """
     Docstring for function_2
     """
-    pass
-    # ...
 
 
 def main():
     """
     Docstring for main
     """
-    # ...
+    wn = turtle.Screen()
+    ourTurtle = turtle.Turtle()
+
+    ourTurtle.shape("turtle")
+
+
     function_1()            # Function call to function_1
     function_2()            # Function call to function_2
+
+    wn.exitonclick()
 
 
 main()
